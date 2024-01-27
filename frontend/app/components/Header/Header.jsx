e7627ae0a0c7dba42105a422ce5b9705fe5e1c61
cocenage <89825630221@mail.ru>
2024-01-27 12:57:49 +0300

    написали сайтик на нексте

---
+++ frontend/app/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '@/app/components/Header/header.module.sass';
+
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+        <h1>негры</h1>
+        <img className={styles.img}  src="./resours/1687374682_chillywilly-club-p-devushka-koshka-art-134.jpg" alt="" />
+    </header>
+  )  
+}
